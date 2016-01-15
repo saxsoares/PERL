9f4cdd345bbe6cd0c3fc88e872b9d88727c385eb
saxsoares <sax.soares@gmail.com>
2016-01-15 14:19:08 -0200

    updt

--- test.pl
+++ test.pl
@@ -1,5 +1,34 @@
 #!/usr/local/bin/perl -w 
 
-$num = "fernando";
-print "um nome" . $num . " um espaço";
-	
+#@array1 = (12,23,34,45,56);
+@a = (44,55,66,undef,234);
+
+if (exists $a[3]){
+	print "True exists\nvalue - $a[3]\n";
+}
+else {
+	print "No exists";
+}
+
+if (defined $a[3]){
+	print "True defined\nvalue - $a[3]\n";
+}
+else {
+	print "No defined\n";
+}
+
+delete $a[3];
+
+if (exists $a[3]){
+	print "True exists\nvaluej - $a[3]\n";
+}
+else {
+	print "No exists\n";
+}
+
+if (defined $a[3]){
+	print "True defined\nvalue - $a[3]\n";
+}
+else {
+	print "No defined\n";
+}
